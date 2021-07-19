5e52b79214de776ab84c85c5c84f3a5b4b3da441
tronglongphung <tronglongphung@gmail.com>
2021-07-20 00:48:32 +0930

    added random id

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,10 +3,20 @@ const notesData = require("../db/db.json");
 // ROUTING
 
 module.exports = (app) => {
-  app.get("/api/notes", (req, res) => res.json(notesData));
+  app.get("/api/notes", (req, res) => {
+    res.json(notesData);
+    console.log(notesData);
+  });
 
   app.post("/api/notes", (req, res) => {
     notesData.push(req.body);
-    res.json(true);
+    res.json(notesData);
+  });
+
+  app.delete("/api/notes/:id", (req, res) => {
+    console.log(notesData);
+
+    notesData.filter(notesData.id);
+    res.json(notesData);
   });
 };
