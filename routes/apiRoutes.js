ab7835367616ca1b0ea09df135d16a393184428a
tronglongphung <tronglongphung@gmail.com>
2021-07-20 22:36:16 +0930

    commit

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,9 @@
 const notesData = require("../db/db.json");
-const fs = require("fs");
+
 // ROUTING
 
 module.exports = (app) => {
   app.get("/api/notes", (req, res) => {
-    fs.readFile("../db/db.json", "utf8");
-
     res.json(notesData);
   });
 
@@ -14,9 +12,9 @@ module.exports = (app) => {
     res.json(notesData);
   });
 
-  // app.delete("/api/notes/:id", (req, res) => {
-  //   const newNote = notesData.filter((note) => note.id !== req.params.id);
-  //   console.log(newNote);
-  //   res.json(notesData);
-  // });
+  app.delete("/api/notes/:id", (req, res) => {
+    const newNote = notesData.filter((note) => note.id !== req.params.id);
+    console.log(newNote);
+    res.json(notesData);
+  });
 };
