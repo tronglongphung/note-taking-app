9c32f5cc08e02f07114212c92977e5b07f1e6921
tronglongphung <tronglongphung@gmail.com>
2021-07-20 22:30:40 +0930

    delete does not work

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,12 @@
 const notesData = require("../db/db.json");
-
+const fs = require("fs");
 // ROUTING
 
 module.exports = (app) => {
   app.get("/api/notes", (req, res) => {
+    fs.readFile("../db/db.json", "utf8");
+
     res.json(notesData);
-    console.log(notesData);
   });
 
   app.post("/api/notes", (req, res) => {
@@ -13,10 +14,9 @@ module.exports = (app) => {
     res.json(notesData);
   });
 
-  app.delete("/api/notes/:id", (req, res) => {
-    console.log(notesData);
-
-    notesData.filter(notesData.id);
-    res.json(notesData);
-  });
+  // app.delete("/api/notes/:id", (req, res) => {
+  //   const newNote = notesData.filter((note) => note.id !== req.params.id);
+  //   console.log(newNote);
+  //   res.json(notesData);
+  // });
 };
